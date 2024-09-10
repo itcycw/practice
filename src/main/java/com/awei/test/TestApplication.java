2a79ad8738725e53526f8920c8da788e8ddc75c2
Alvis <alvis@difft.org>
2024-09-10 21:26:00 +0800

    ceshi

--- src/main/java/com/awei/test/TestApplication.java
+++ src/main/java/com/awei/test/TestApplication.java
@@ -3,6 +3,9 @@ package com.awei.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author awei
+ */
 @SpringBootApplication
 public class TestApplication {
 
